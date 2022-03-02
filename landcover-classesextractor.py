abd2d7e7951156484ad722681bc7d43f0e310269
Christos Charisis <christos@scio.systems>
2022-03-02 13:26:15 +0200

    initial commit

---
+++ landcover-classesextractor.py
@@ -0,0 +1,143 @@
+import numpy as np
+import numpy.ma as ma
+import gdal
+import os
+import boto3
+from botocore.exceptions import ClientError
+import json
+import logging
+# import sys
+
+s3 = boto3.client('s3')
+
+
+
+#%%
+def lambda_handler(event, context):
+    
+    body = json.loads(event['body'])
+    json_file = body
+    
+    #get  input json and extract geojson
+    try:
+        project_id = json_file["project_id"]
+        if "land_use_map" in json_file:
+            if json_file["land_use_map"]["custom_map_url"]!="n/a":
+                default=False
+            else:
+                default=True
+        else:
+            default=True
+        
+    except Exception as e:
+        print("Input JSON field have an error.")
+        return {
+            "statusCode": 400,
+            "body": e
+        }
+
+    #for local
+    # path_to_tmp = "/home/christos/Desktop/SCiO_Projects/lup4ldn/data/cropped_files/"
+    #for aws
+    path_to_tmp = "/tmp/"
+
+    s3_lambda_path = '/vsis3/lup4ldn-lambdas/'
+    
+    
+    ## DEFAULT CASE: CLASSES from land cover
+    if default:
+        #must use gdal.Open in order to fill the file created from gdal.Warp, else the file remaines full of nodata
+        try:
+            land_cover_tif = gdal.Open(s3_lambda_path + project_id + "/cropped_land_cover.tif")
+            land_cover_array = land_cover_tif.ReadAsArray()
+        except Exception as e:
+            print(e)
+            print("if ''NoneType' object has no attribute', probably the file path is wrong")
+        
+        
+        def map_land_cover_to_trendsearth_labels(array,labels_dict):
+            for key in labels_dict:
+                array = np.where(array==key,labels_dict[key],array)
+            return array
+        dict_labels_map_100m_to_trends = {
+            10 : 3,
+            11 : 3,
+            12 : 3,
+            20 : 3,
+            30 : 3,
+            40 : 2,
+            50 : 1,
+            60 : 1,
+            61 : 1,
+            62 : 1,
+            70 : 1,
+            71 : 1,
+            72 : 1,
+            80 : 1,
+            81 : 1,
+            82 : 1,
+            90 : 1,
+            100 : 1,
+            110 : 2,
+            120 : 2,
+            121 : 2,
+            122 : 2,
+            130 : 2,
+            140 : 2,
+            150 : 2,
+            151 : 2,
+            152 : 2,
+            153 : 2,
+            160 : 4,
+            170 : 4,
+            180 : 4,
+            190 : 5,
+            200 : 6,
+            201 : 6,
+            202 : 6,
+            210 : 7,
+            220 : 6,
+            0 : -32768
+        }
+        
+        land_cover_array = map_land_cover_to_trendsearth_labels(land_cover_array,dict_labels_map_100m_to_trends)
+        unique, counts = np.unique(land_cover_array, return_counts = True)
+        if -32768 in unique:
+            unique = unique[1:]
+            counts = counts[1:]
+        lc_hectares = dict(zip([str(x) for x in unique],  [9 * int(x) for x in counts]))
+    
+    ## CUSTOM CASE: CLASSES from land use
+    else:
+        #must use gdal.Open in order to fill the file created from gdal.Warp, else the file remaines full of nodata
+        try:
+            t = gdal.Open(s3_lambda_path + project_id + "/cropped_land_use.tif")
+        except Exception as e:
+            print("if ''NoneType' object has no attribute', probably the file path is wrong")
+            return {
+                "statusCode": 500,
+                "body": e
+            }
+            
+        land_use_array = t.ReadAsArray()
+        land_use_array = np.where(land_use_array<=0, -32768,land_use_array)
+        
+        unique, counts = np.unique(land_use_array, return_counts = True)
+        if -32768 in unique:
+            unique = unique[1:]
+            counts = counts[1:]
+        
+        lc_hectares = dict(zip([str(x) for x in unique],  [9*int(x) for x in counts]))
+
+    
+
+    my_output = {
+    "land_cover_hectares_per_class" : lc_hectares
+    }
+    
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps(my_output)
+    }
+    
